8a5c34fed5eedb473ab5283774d94fd7f0d7d8d4
NovaDileep <nova.cs17@jecc.ac.in>
2021-04-20 17:21:45 +0530

    changed

---
+++ todoapp/todo.js
@@ -0,0 +1,46 @@
+state=false;
+function check(){
+    user=document.getElementById("user");
+    error=document.getElementById("invalid");
+    password=document.getElementById("password");
+    wrongp=document.getElementById("wrongpass");
+    if(user.name=='admin' && password.value=='12345'){
+       return true;
+    }
+    
+    if(user.value!='admin'){
+      error.innerHTML="Invalid Username";
+      error.style.color="red";
+      user.style.border="1px solid red";
+     }
+  else{
+      error.textContent="Valid Username";
+      error.style.color="green";
+      user.style.border="1px solid green";
+    }
+   if(password.value!='12345'){
+      wrongp.innerHTML="Wrong password";
+      wrongp.style.color="red";
+      password.style.border="red";
+   }
+   else{
+      wrongp.innerHTML=" ";
+      password.style.border="1px solid green";
+   }
+   return false;
+   }
+
+
+function myFunction(show){
+   show.classList.toggle('fa-eye-slash');
+}
+function toggle(){
+   if(state){
+     password.setAttribute("type","password");
+     state=false;
+   }
+   else{
+      password.setAttribute("type","text");
+      state=true;
+   }
+} 
